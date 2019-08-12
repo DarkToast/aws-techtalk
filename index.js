1488920cf9db867932607333cfd9b6eccbb18443
Christian Iwanzik <c.iwanzik@tarent.de>
2019-08-12 14:56:43 +0200

    Adding a test

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 async function handlePut(event, context) {
     console.log(`Got event for handlePut: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200
+    }
 }
 
 async function handleGet(event, context) {
     console.log(`Got event for handleGet: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200
+    }
 }
 
 module.exports = {
