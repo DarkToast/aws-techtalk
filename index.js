f370fe9954bd086e32fb51b8c39a695160a7c649
Désirée Amling <d.amling@tarent.de>
2019-08-14 15:38:51 +0200

    Added database and calls to said database.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-central-1'});
+const awsDynamoDb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const KeyStoreDatabase = require('./src/key-store-db');
+const keyStoreDb = KeyStoreDatabase(awsDynamoDb, process.env.KEY_STORE_REF);
+
 async function handlePut(event, context) {
     console.log(`Got event for handlePut: ${JSON.stringify(event)}`);
+
+    await keyStoreDb.save(event);
+
     return {
         statusCode: 200
     }
@@ -7,8 +16,12 @@ async function handlePut(event, context) {
 
 async function handleGet(event, context) {
     console.log(`Got event for handleGet: ${JSON.stringify(event)}`);
+
+    let result = await keyStoreDb.load(event.key);
+
     return {
-        statusCode: 200
+        statusCode: 200,
+        body: JSON.stringify(result)
     }
 }
 
---
+++ src/key-store-db.js
@@ -0,0 +1,48 @@
+let KeyStoreDb = function KeyStoreDb(dynamoDb, tableName) {
+    let load = async function load(key) {
+        const params = {
+            TableName: tableName,
+            Key: {
+                key: {
+                    S: key
+                }
+            }
+        };
+
+        return dynamoDb.getItem(params).promise()
+            .then(data => {
+                if (data.Item) {
+                    console.log('Loaded item from database!');
+
+                    return {
+                        key: data.Item.key.S,
+                        value: data.Item.value.S
+                    };
+                }
+
+                console.log(`Item with the key ${key} does not exist in the database`);
+            });
+    };
+
+    let save = async (item) => {
+        let params = {
+            TableName: tableName,
+            Item: {
+                key: {S: item.key},
+                value: {S: item.value}
+            }
+        };
+
+        dynamoDb.putItem(params).promise()
+            .then(function() {
+                console.log('Saved item to database.');
+            });
+    };
+
+    return {
+        load,
+        save
+    };
+};
+
+module.exports = KeyStoreDb;
